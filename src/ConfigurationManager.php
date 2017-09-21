6c984a86cc5b0b47b1128b4125d0a0648d3be050
Michael O'Connell <moconnell@lifestylepubs.com>
2017-09-21 10:58:40 -0500

    add static created to make creation a bit easier add read method to make reading a file easy add all method to get all configuration

--- src/ConfigurationManager.php
+++ src/ConfigurationManager.php
@@ -5,6 +5,10 @@ namespace Config;
 use Config\Exception\ConfigurationFileException;
 use Config\Exception\ConfigurationMissingException;
 use Config\FileReader\FileReader;
+use Config\FileReader\JsonReader;
+use Config\FileReader\PhpReader;
+use Config\FileReader\YamlReader;
+use Config\FileReader\DirectoryReader;
 use Psr\SimpleCache\CacheInterface;
 
 /**
@@ -12,6 +16,8 @@ use Psr\SimpleCache\CacheInterface;
  */
 class ConfigurationManager
 {
+    const DEFAULT_SEPARATOR = '/';
+
     /**
      * @var array
      */
@@ -35,12 +41,35 @@ class ConfigurationManager
      * @param FileReader[] $fileReaders
      * @param string $separator
      */
-    public function __construct(array $fileReaders, string $separator = '/')
+    public function __construct(array $fileReaders, string $separator = self::DEFAULT_SEPARATOR)
     {
         foreach ($fileReaders as $fileReader) {
             $this->addFileReader($fileReader);
         }
         $this->separator = $separator;
+    }
+
+    public static function create(array $fileReaders = [], $separator = self::DEFAULT_SEPARATOR, bool $prefixFiles = true): self
+    {
+        if (empty($fileReaders)) {
+            $fileReaders = self::allReaders();
+        }
+        $config = new self($fileReaders, $separator);
+        $directoryReader = new DirectoryReader($config, $prefixFiles);
+        $config->addFileReader($directoryReader);
+        return $config;
+    }
+
+    /**
+     * @return FileReader[]
+     */
+    protected static function allReaders(): array
+    {
+        $readers = [new PhpReader(), new JsonReader()];
+        if (class_exists('Symfony\\Component\\Yaml\\Parser')) {
+            $readers[] = new YamlReader();
+        }
+        return $readers;
     }
 
     /**
@@ -88,6 +117,27 @@ class ConfigurationManager
     }
 
     /**
+     * Retrieves all configuration information as an array
+     *
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->config;
+    }
+
+    /**
+     * Reads all configuration from the provided path
+     *
+     * @param string $path Full path to file or directory
+     */
+    public function read(string $path)
+    {
+        $reader = $this->getFileReader($path);
+        $this->addConfiguration($reader->read($path));
+    }
+
+    /**
      * Adds configuration, configuration added later override values provided from previous sources
      *
      * @param array $config
@@ -108,16 +158,21 @@ class ConfigurationManager
     }
 
     /**
-     * @param string $fileName A file path, the extension is used to determine which file reader is loaded
+     * @param string $path A file path, the extension is used to determine which file reader is loaded
      * @return FileReader
      */
-    public function getFileReader(string $fileName): FileReader
+    public function getFileReader(string $path): FileReader
     {
-        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
-        if (isset($this->fileReaders[$extension])) {
-            return $this->fileReaders[$extension];
+        if (is_dir($path)) {
+            $key = '/';
+        } else {
+            $key = pathinfo($path, PATHINFO_EXTENSION);
         }
-        throw new ConfigurationFileException($fileName, 0, null, ' no file reader available');
+
+        if (isset($this->fileReaders[$key])) {
+            return $this->fileReaders[$key];
+        }
+        throw new ConfigurationFileException($path, 0, null, " no file reader for {$key} available");
     }
 
     /**
--- tests/ConfigurationManagerTest.php
+++ tests/ConfigurationManagerTest.php
@@ -3,7 +3,11 @@
 namespace Config\Test;
 
 use Config\ConfigurationManager;
+use Config\Exception\ConfigurationFileException;
+use Config\FileReader\DirectoryReader;
+use Config\FileReader\JsonReader;
 use Config\FileReader\PhpReader;
+use Config\FileReader\YamlReader;
 
 class ConfigurationManagerTest extends BaseTestCase
 {
@@ -39,12 +43,71 @@ class ConfigurationManagerTest extends BaseTestCase
         $this->assertInstanceOf(PhpReader::class, $reader);
     }
 
+    public function testGetDirectoryReader()
+    {
+        $config = new ConfigurationManager([new PhpReader()]);
+        $config->addFileReader(new DirectoryReader($config));
+        $reader = $config->getFileReader($this->getConfigDir());
+        $this->assertInstanceOf(DirectoryReader::class, $reader);
+    }
+
     /**
      * @expectedException \Config\Exception\ConfigurationFileException
      */
     public function testGetMissingFileReader()
     {
         $config = new ConfigurationManager([new PhpReader()]);
-        $reader = $config->getFileReader('dir/file.toml');
+        $config->getFileReader('dir/file.toml');
+    }
+
+    public function testCreate()
+    {
+        $config = ConfigurationManager::create();
+        $this->assertInstanceOf(ConfigurationManager::class, $config);
+
+        //test that all readers have been added
+        $reader = $config->getFileReader('dir/file.php');
+        $this->assertInstanceOf(PhpReader::class, $reader);
+        $reader = $config->getFileReader('dir/file.yaml');
+        $this->assertInstanceOf(YamlReader::class, $reader);
+        $reader = $config->getFileReader('dir/file.json');
+        $this->assertInstanceOf(JsonReader::class, $reader);
+        $reader = $config->getFileReader($this->getConfigDir());
+        $this->assertInstanceOf(DirectoryReader::class, $reader);
+    }
+
+    public function testCreateWithReaders()
+    {
+        $config = ConfigurationManager::create([new PhpReader()]);
+        $this->assertInstanceOf(ConfigurationManager::class, $config);
+
+        //test that all readers have been added
+        $reader = $config->getFileReader('dir/file.php');
+        $this->assertInstanceOf(PhpReader::class, $reader);
+
+        try {
+            $config->getFileReader('dir/file.yaml');
+            $this->fail('Expected ConfigurationFileException');
+        } catch (ConfigurationFileException $e) {
+            $this->addToAssertionCount(1);
+        }
+    }
+
+    public function testReadFile()
+    {
+        $config = ConfigurationManager::create([new PhpReader()]);
+        $config->read($this->getConfigDir() . 'phpfile.php');
+        $this->assertEquals($this->getSimpleTestData(), $config->all());
+    }
+
+    public function testReadDirectory()
+    {
+        $config = ConfigurationManager::create();
+        $config->read($this->getConfigDir());
+        $this->assertEquals($this->getSimpleTestData(), $config->get('phpfile'));
+        $this->assertEquals($this->getSimpleTestData(), $config->get('yamlfile'));
+        $this->assertEquals($this->getSimpleTestData(), $config->get('jsonfile'));
+        $this->assertEquals($this->getSimpleTestData(), $config->get('subdir/phpfile'));
+        $this->assertEquals($this->getSimpleTestData(), $config->get('subdir/subsubdir/phpfile'));
     }
 }
