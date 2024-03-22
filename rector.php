9fe714526e535d753043d8d465f36ca991c3d7e7
Michael O'Connell <moconnell@citylifestyle.com>
2024-03-22 10:57:51 -0500

    add phpunit 10 ruleset to rector

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
@@ -11,6 +12,9 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     ->withPhpSets()
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_100
+    ])
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
     ]);
